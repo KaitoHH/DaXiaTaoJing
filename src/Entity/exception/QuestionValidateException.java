e1e441df4af155c6e75c23861e4ee0d7e0671c93
KaitoHH <hh.kaito@gmail.com>
2016-11-03 16:41:09 +0800

    post_question tag & database

---
+++ src/Entity/exception/QuestionValidateException.java
@@ -0,0 +1,11 @@
+package Entity;
+
+/**
+ * Project: DaXiaTaoJing
+ * Author: KaitoHH
+ * Create Date: 2016/11/3
+ * Description:
+ * All rights reserved.
+ */
+public class QuestionValidateException {
+}
---
+++ src/database/IQuestionDAO.java
@@ -0,0 +1,11 @@
+package database;
+
+/**
+ * Project: DaXiaTaoJing
+ * Author: KaitoHH
+ * Create Date: 2016/11/3
+ * Description:
+ * All rights reserved.
+ */
+public class IQuestionDAO {
+}
